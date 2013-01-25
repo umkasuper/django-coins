299b6a542faf4c0d58c708ad7bdfbe0537a2e469
Maksim Kolesnikov <maksim@m-200.com>
2013-01-25 15:56:08 +0400

    Список стран берется из базы

--- euro/views.py
+++ euro/views.py
@@ -3,20 +3,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from euro.models import Coins
+from euro.models import Country
 
 # Create your views here.
-class coins_object():
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
 def coins(request):
     if 'country' in request.GET:
-#        message = 'You searched for %s' % request.GET['country']
-#        key_name = ['1c', '2c', '5c', '10c', '20c', '50c', '1€', '2€']
         coins = Coins.objects.filter(country__name = request.GET['country']).order_by('nominal')
-	return render_to_response('coins.html',  {'coins': coins, 'query': request.GET['country']})
+        country = Country.objects.all().order_by('name')
+	return render_to_response('coins.html',  {'coins': coins, 'query': request.GET['country'], 'country': country})
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
