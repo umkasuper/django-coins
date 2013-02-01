955cbdbe4fc40b4eb5e37429c8b0a9dc67de1407
Maksim Kolesnikov <maksim@m-200.com>
2013-02-01 16:33:28 +0400

    Выборка нескольких стран, и выборка по имеющимся, неимеющимся

--- euro/templatetags/user_coin.py
+++ euro/templatetags/user_coin.py
@@ -5,7 +5,7 @@ from django import template
 register = template.Library()
 
 def user_have_coin(user, coin):
-    return u"Есть" if coin.coin_owner.filter(username = user.username) else u"Нет"
+    return u"Есть" if coin.coin_owner.filter(username = user.username) else None
 
 
 register.filter('user_have_coin', user_have_coin)
--- euro/views.py
+++ euro/views.py
@@ -5,7 +5,17 @@ from django.shortcuts import render_to_response
 from euro.models import Coins
 from euro.models import Country
 
-# Create your views here.
+class countryCoins():
+    coins_group = []
+    country_name = ""
+
+    def __init__(self, country_name):
+        self.country_name = country_name
+        self.coins_group = []
+
+    def add_coin_group(self, coin_group):
+        self.coins_group.append(coin_group)
+
 class groupCoin():
 
     def __init__(self, coins_group, coins_group_name):
@@ -16,27 +26,32 @@ class groupCoin():
 def coins(request):
     if request.user.is_authenticated():
         if 'country' in request.GET:
-                print request.user.username
+            print request.user.username
 
-                request_country = request.GET['country']
-                country = Country.objects.filter(coin_group__group_name__in=['euro', 'normal']).order_by('name'). distinct()
+            request_country_list = request.GET['country'].split(",")
+            grouped_country = []
+
+            country = Country.objects.filter(coin_group__group_name__in=['euro', 'normal']).order_by('name'). distinct()
+            for request_country in request_country_list:
+                group_country = countryCoins(request_country)
                 coins_of_country = Coins.objects.filter(country__name = request_country)
 
                 if coins_of_country:
                     type_of_coins = Country.objects.filter(name = request_country)[0].coin_group.all().exclude(group_name__in=['euro', 'normal'])
 
-                    coins_group = []
                     # есть типы кроме euro, и стандарт (есть ли другие выпуски?)
                     if type_of_coins:
                         for coins_group_name in type_of_coins.values_list('group_name', flat=True):
                             coins = coins_of_country.filter(coin_group__group_name = coins_group_name).order_by('coin_group', 'nominal')
-                            coins_group.append(groupCoin(coins, coins_group_name.split()[-1]))
+                            group_country.add_coin_group(groupCoin(coins, coins_group_name.split()[-1]))
                     else:
-                        coins_group.append(groupCoin(coins_of_country.order_by('nominal'), "euro"))
+                        group_country.add_coin_group(groupCoin(coins_of_country.order_by('nominal'), ""))
 
-                    return render_to_response('coins.html',  {'request': request, 'query': request_country, 'country': country, 'coins_group': coins_group})
-                else:
-                    message = 'Coins country %s not found.' % request.GET['country']
+                grouped_country.append(group_country)
+
+            return render_to_response('coins.html',  {'request': request, 'query': request_country, 'country': country, 'grouped_country': grouped_country})
+#                    else:
+#                        message = 'Coins country %s not found.' % request.GET['country']
         else:
                 message = 'You submitted an empty form.'
     else:
