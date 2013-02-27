78ab6ddf38cf60c7705ef4431669a98dbc36ccf6
Maksim Kolesnikov <maksim@m-200.com>
2013-02-27 10:18:58 +0400

    Регистрация и восстановление пароля пользователя

--- coins/urls.py
+++ coins/urls.py
@@ -3,6 +3,7 @@
 
 import settings
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from euro.views import euro, memorable
 from euro.views import usa
@@ -10,27 +11,28 @@ from euro.views import russia
 from euro.views import set_coins
 from euro.sys   import site_logout
 
+
+from registration.views import register, activate
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'coins.views.home', name='home'),
-    # url(r'^coins/', include('coins.foo.urls')),
-
     url(r'^euro/$', 'euro.views.euro', name='euro'),
     url(r'^euro/memorable/(?P<country>\S+)$', memorable, {'selectors': ["euro", "memorable"]}, name='memorable'),
     url(r'^set/$', set_coins),
     url(r'^usa/$', usa),
     url(r'^russia/$', russia),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', site_logout),
-#    url(r'^accounts/profile/$', 'django.contrib.auth.views.profile'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'', include('registration.auth_urls')),
 )
