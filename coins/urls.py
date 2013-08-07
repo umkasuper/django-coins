7e5b5f9468e027bcc2fee2f68de51f9809d70173
Maksim Kolesnikov <maksim@m-200.com>
2013-08-07 09:50:32 +0400

    Убран линшний import

--- coins/urls.py
+++ coins/urls.py
@@ -8,10 +8,10 @@ from django.views.generic import TemplateView
 from euro.views import euro, memorable
 from euro.views import all_memorable
 from euro.views import set_coins
-from euro.sys   import site_logout
+from euro.sys import site_logout
 
 
-from registration.views import register, activate
+#from registration.views import register, activate
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,6 +20,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^euro/$', 'euro.views.euro', name='euro'),
+    url(r'^euro/memorable/(?P<year>\d{4})$', memorable, {'selectors': ["euro", "memorable"], 'country': "all"}, name='year'),
     url(r'^euro/memorable/(?P<country>\S+)$', memorable, {'selectors': ["euro", "memorable"]}, name='memorable'),
     #url(u'^(?P<country>\S+)/memorable/(?P<type>\S+)/$', usa, {'selectors': ['memorable']}, name='country_memorable'),
     url(u'^(?P<country>\S+)/memorable/(?P<type>[-\w\ ]+)/$', all_memorable, {'selectors': ['memorable']}, name='country_memorable'),
