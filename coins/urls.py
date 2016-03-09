062784bcd4430637e8bc5c573b4ddaefafe4a4de
Maksim Kolesnikov <maksim@m-200.com>
2016-03-09 10:57:52 +0300

    Добавление api

--- coins/urls.py
+++ coins/urls.py
@@ -10,7 +10,7 @@ from euro.views import all_memorable
 from euro.views import set_coins
 
 from euro.sys import site_logout
-
+from euro.api_v1 import api_v1_euro_list_countries, api_v1_euro_list_years, api_v1_euro_what
 
 #from registration.views import register, activate
 
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
     url(r'^pdf/euro/memorable/(?P<year>\d{4})$', memorable, {'selectors': ["euro", "memorable"], 'country': "all", "pdf" :True}, name='pdf_year'),
     url(r'^pdf/euro/memorable/(?P<country>\S+)$', memorable, {'selectors': ["euro", "memorable"], "pdf": True}, name='pdf_memorable'),
 
-    url(r'^$', memorable, {'selectors': ["euro", "memorable"], 'country': "all", 'year': '2014'}, name='year'),
+    url(r'^$', memorable, {'selectors': ["euro", "memorable"], 'country': "all", 'year': '2015'}, name='year'),
 
     url(r'^euro/$', 'euro.views.euro', name='euro'),
     url(r'^euro/memorable/(?P<year>\d{4})$', memorable, {'selectors': ["euro", "memorable"], 'country': "all"}, name='year'),
@@ -31,6 +31,10 @@ urlpatterns = patterns('',
     #url(u'^(?P<country>\S+)/memorable/(?P<type>\S+)/$', usa, {'selectors': ['memorable']}, name='country_memorable'),
     url(u'^(?P<country>\S+)/memorable/(?P<type>[-\w\ ]+)/$', all_memorable, {'selectors': ['memorable']}, name='country_memorable'),
     url(r'^set/$', set_coins),
+
+    url(r'^api/v1/euro/list_countries', api_v1_euro_list_countries),
+    url(r'^api/v1/euro/list_years', api_v1_euro_list_years),
+    url(r'^api/v1/euro/what', api_v1_euro_what),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
